0044dac4927bf88d4362643ac3835da092ad0ef8
Ludvig Gunne Lindström <ludd.lindstroem@gmail.com>
2023-04-15 09:44:06 +0200

    added license

--- mprof.hpp
+++ mprof.hpp
@@ -1,4 +1,27 @@
 
+/*
+ * mprof - single header C++ library for collecting and emitting profiling data
+ * Copyright (C) 2023 Ludvig Gunne Lindström
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+*/
+
 #ifndef MPROF_HEADER
 #define MPROF_HEADER
 
@@ -47,7 +70,6 @@ namespace mprof {
 
         ~collector_t()
         {
-
             result.total_cycles = __rdtsc() - result.total_cycles ;
             result_handler(result);
             for (auto accumulator : result.accumulators) {
